ed520df7dee8ec12e3f3e61c4af4c0af1ab17849
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-10 17:40:55 +0545

    chore: added UICollectionViewController and created some layout for image and label using InterfaceBuilder

---
+++ project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Waterflow Technology on 10/04/2025.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
